6f85c81983ea59edfb0db823e5b1dffdf3187022
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-02-09 19:26:34 +0100

    Add files via upload

---
+++ kolekcijaUnosa/kolekcijaUnosa/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace kolekcijaUnosa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.textBoxGodina = new System.Windows.Forms.TextBox();
+            this.comboBoxrod = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PREZIME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GODINA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SPOL";
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(78, 23);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(100, 22);
+            this.textBoxIme.TabIndex = 4;
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(78, 54);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPrezime.TabIndex = 5;
+            // 
+            // textBoxGodina
+            // 
+            this.textBoxGodina.Location = new System.Drawing.Point(78, 81);
+            this.textBoxGodina.Name = "textBoxGodina";
+            this.textBoxGodina.Size = new System.Drawing.Size(100, 22);
+            this.textBoxGodina.TabIndex = 6;
+            this.textBoxGodina.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // comboBoxrod
+            // 
+            this.comboBoxrod.FormattingEnabled = true;
+            this.comboBoxrod.Items.AddRange(new object[] {
+            "M",
+            "Ž"});
+            this.comboBoxrod.Location = new System.Drawing.Point(78, 109);
+            this.comboBoxrod.Name = "comboBoxrod";
+            this.comboBoxrod.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxrod.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 169);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(775, 269);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 111);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "UNESI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(414, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 111);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "OBRADI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(613, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 111);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "ISPIŠI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBoxrod);
+            this.Controls.Add(this.textBoxGodina);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+        private System.Windows.Forms.TextBox textBoxGodina;
+        private System.Windows.Forms.ComboBox comboBoxrod;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
